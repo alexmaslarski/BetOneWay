052d927632fab6e528d8699b8841dcf00b78989b
alexmaslarski <38736057+alexmaslarski@users.noreply.github.com>
2020-05-12 13:19:20 +0300

    tipsters

--- src/components/Tipsters/TipsterListing.vue
+++ src/components/Tipsters/TipsterListing.vue
@@ -3,16 +3,26 @@
     <v-card v-for="tipster in tipsters" :key="tipster.id" class="mx-auto mb-3" color="white">
         <router-link :to="{ name: 'Profile', params: { id: tipster.id }}" tag="div">
       <v-list-item two-line>
-        <v-list-item-avatar>
+        <v-list-item-avatar class="align-self-start">
           <img :src="tipster.photoUrl" alt="">
         </v-list-item-avatar>
         <v-list-item-content>
           <v-list-item-title>{{tipster.name}}</v-list-item-title>
+          <div class="d-flex mb-2">
+          <p class="caption mb-0"><v-icon class="mr-1" size="12">mdi-account-group</v-icon>{{tipster.followers && tipster.followers.length > 0 ? tipster.followers.length : '0'}}</p>
+          <p class="caption mb-0 ml-4"> {{tipster.posts && tipster.posts.length > 0 ? tipster.posts.length : '0'}} tips</p>
+          </div>
           <div class="d-flex">
-          <v-list-item-subtitle class="caption"><v-icon size="12">mdi-account-group</v-icon> {{tipster.followers && tipster.followers.length > 0 ? tipster.followers.length : '0'}}</v-list-item-subtitle>
-          <v-list-item-subtitle class="caption"><v-icon size="12">mdi-account-group</v-icon> {{tipster.followers && tipster.followers.length > 0 ? tipster.followers.length : '0'}}</v-list-item-subtitle>
+          <p class="subtitle-2 success--text font-weight-medium mb-0"><v-icon color="success" class="mr-1" size="12">mdi-chart-line-variant</v-icon>{{tipster.totalProfit.toFixed(2)}}u</p>
+          <p class="subtitle-2 primary--text font-weight-medium mb-0 ml-4"><v-icon color="primary" class="mr-1" size="12">mdi-shield</v-icon>{{tipster.yield.toFixed(2)}}u</p>
           </div>
         </v-list-item-content>
+        <v-list-item-action class="align-self-stretch d-flex flex-column">
+          <p class="caption mb-0">{{tipster.avgRating}}<v-icon size="12" class="ml-1" color="accent">mdi-star</v-icon></p>
+          <v-btn @click="handleSubscribe" small rounded class="mr-2 justify-self-center"
+            :class="[isSubscribed ? 'secondary lighten-1 secondary--text' : 'primary darken-1']"
+            >{{isSubscribed ? 'Subscribed' : '$59.99'}}</v-btn>
+        </v-list-item-action>
         </v-list-item>
         </router-link>
     </v-card>
